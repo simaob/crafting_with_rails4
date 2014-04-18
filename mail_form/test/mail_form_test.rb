138dbf90be8870da81fdcb23f838ecb0fb990cbf
Simao Belchior de Castro <simaobelchior@gmail.com>
2014-04-18 17:02:43 +0100

    Added mail form chapter

---
+++ mail_form/test/mail_form_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+require 'fixtures/sample_mail'
+
+class MailFormTest < ActiveSupport::TestCase
+  test 'sample mail has name and email as attributes' do
+    sample = SampleMail.new
+    sample.name = 'User'
+    assert_equal 'User', sample.name
+    sample.email = 'user@example.com'
+    assert_equal 'user@example.com', sample.email
+  end
+
+  test 'sample mail can clear attributes using clear_ prefix' do
+    sample = SampleMail.new
+
+    sample.name = 'User'
+    sample.email = 'user@example.com'
+    assert_equal 'User', sample.name
+    assert_equal 'user@example.com', sample.email
+
+    sample.clear_name
+    sample.clear_email
+    assert_nil sample.name
+    assert_nil sample.email
+  end
+end
